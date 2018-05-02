7a5a0700930bb64b5fe30616248734ac3c1e6119
Ning Sun <sunng@protonmail.com>
2018-05-02 13:29:29 +0800

    (cargo-release) generate docs

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["handlebars"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"handlebars/struct.Template.html\" title=\"struct handlebars::Template\">Template</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"handlebars/struct.Context.html\" title=\"struct handlebars::Context\">Context</a>",];
+implementors["handlebars"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"handlebars/struct.Template.html\" title=\"struct handlebars::Template\">Template</a>",synthetic:false,types:["handlebars::template::Template"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"handlebars/struct.Context.html\" title=\"struct handlebars::Context\">Context</a>",synthetic:false,types:["handlebars::context::Context"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["handlebars"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"handlebars/struct.StringWriter.html\" title=\"struct handlebars::StringWriter\">StringWriter</a>",];
+implementors["handlebars"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"handlebars/struct.StringWriter.html\" title=\"struct handlebars::StringWriter\">StringWriter</a>",synthetic:false,types:["handlebars::support::str::StringWriter"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
